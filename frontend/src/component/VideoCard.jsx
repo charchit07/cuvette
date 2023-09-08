bd2497dd07f27b414787cf6f81ecedc6e0000817
charchit <charchitkhandelwal45@gmail.com>
2023-09-08 21:06:06 +0530

    added

--- frontend/src/component/VideoCard.jsx
+++ frontend/src/component/VideoCard.jsx
@@ -4,7 +4,7 @@ import styles from "../Styles/VideoCard.module.css";
 const VideoCard = ({ video }) => {
   return (
     <div className={styles.card}>
-      <iframe title="Fitness Video" src={video.video}></iframe>
+     <video title="Fitness Video" src={video.video} controls={true} autoPlay={false}></video>
       <h2>{video.queryHeading}</h2>
       <p>Tags: {video.tags.join(", ")}</p>
     </div>
